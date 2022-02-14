c6b13da179d2cccaacd46b02f0cba68047ff7853
anthonykizer <anthony.kizer88@gmail.com>
2022-02-14 11:07:42 -0800

    added error handling for existing files and unaccepted flags

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const genFiles = require("./lib/genFiles")
+const arguments = require("./errors/arguments")
+
+if (!process.argv[2]) throw new Error("Must provide an entity as an argument")
+
+const args = process.argv.slice(2);
+
+args.forEach(arg => {
+    arguments.forEach(item => {
+        if (item.check(arg)) item.error(arg);
+    })
+})
 
 for (let i = 2; i < process.argv.length; i++) {
     const entityName = process.argv[i];
