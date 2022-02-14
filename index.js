e1168fbcf8685f5a82830c0755e63f8985e3ee69
anthonykizer <anthony.kizer88@gmail.com>
2022-02-14 10:29:22 -0800

    added uncaughtException handlker

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ for (let i = 2; i < process.argv.length; i++) {
     const entityName = process.argv[i];
     genFiles(entityName)
 }
+
+process.on('uncaughtException', err => {
+    console.error('There was an uncaught error', err)
+    process.exit(1)
+})
